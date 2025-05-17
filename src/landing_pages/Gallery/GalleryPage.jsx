ee65d1ddd666f6bd05e7fadf79929a6d1845cfb9
Sadique Shakeel <tempsadique@gmail.com>
2025-05-17 15:49:22 +0530

    Intial Phase Done

--- src/landing_pages/Gallery/GalleryPage.jsx
+++ src/landing_pages/Gallery/GalleryPage.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import Hero from './Hero';
 import Images from './RenderImages';
+import "./Gallery.css"
+
+const imageList =[
+     "/media/images/img1.webp",
+    "/media/images/img2.webp",
+    "/media/images/img3.webp",
+    "/media/images/img4.webp",
+    "/media/images/img5.webp",
+    "/media/images/img6.webp",
+    "/media/images/img7.webp",
+    "/media/images/img8.webp",
+    "/media/images/img9.webp",
+    "/media/images/img10.webp",
+    "/media/images/img12.webp",
+    "/media/images/img1.webp",
+    "/media/images/gl1.png",
+    "/media/images/gl2.png",
+    "/media/images/gl3.png",
+    "/media/images/gl4.png",
+    "/media/images/gl5.png",
+    "/media/images/gl6.png",
+    "/media/images/gl7.png",
+    "/media/images/gl8.png",
+];
 
 function GalleryPage() {
     return (<>
         <Hero   title="GALLERY"
   backgroundImage="https://images.unsplash.com/photo-1507643179773-3e975d7ac515?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-        <Images/>
+        <Images images={imageList} />
     </> );
 }
 
--- src/landing_pages/Gallery/RenderImages.jsx
+++ src/landing_pages/Gallery/RenderImages.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Container, Row, Col } from "react-bootstrap";
 
-function Images() {
-    return ( <>
-    <h1>Images</h1>
-    </> );
-}
+const PhotoGallery = ({ images = [] }) => {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
 
-export default Images;
+  return (
+    <div style={{ padding: "60px 0", backgroundColor: "#fffaf4" }}>
+      <Container>
+        <Row>
+          {images.map((image, index) => (
+            <Col
+              key={index}
+              md={4}
+              sm={6}
+              xs={12}
+              className="mb-4"
+              data-aos="zoom-in"
+            >
+              <div className="gallery-img-wrapper">
+                <img
+                  src={image}
+                  alt={`gallery-${index}`}
+                  className="gallery-img"
+                />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default PhotoGallery;
